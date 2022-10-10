cdef32079a90cd023df2be3e95c85d52c8141ea5
Icaro Albar <icaro.albar@live.com>
2022-10-10 14:10:24 -0300

    Estrutura de SEO configurada

---
+++ components/SeoConfig.jsx
@@ -0,0 +1,7 @@
+const title = 'Plano de Saúde Amil Rio'
+const description = 'Todos os planos de saúde amil e simulador de planos de saúde online'
+
+const SEO = {
+    title,
+    description
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Page from '../components/Page'
+// import Nav from '../components/Nav'
+// import Header from '../components/Header'
+// import Footer from '../components/Footer'
+// import Contato from '../components/Contato'
 
 export default function Home() {
+
   return (
-    <>
-    <h1 className='text-red-500'>teste</h1>
-    </>
-  ) 
+    <Page title="Plano de Saúde Amil Rio" description="Todos os planos de saúde amil e simulador de planos de saúde online" path="/">
+
+    </Page>
+  )
 }
