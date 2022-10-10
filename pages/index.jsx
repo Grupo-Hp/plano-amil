4efcb0c4192f3d4d9d48e53e9934c3fbae0d4c62
Icaro Albar <icaro.albar@live.com>
2022-10-10 14:37:35 -0300

    Footer configurado

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import Page from '../components/Page'
 import Nav from '../components/Nav'
 // import Header from '../components/Header'
-// import Footer from '../components/Footer'
+import Footer from '../components/Footer'
 // import Contato from '../components/Contato'
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <Page title="Plano de Saúde Amil Rio" description="Todos os planos de saúde amil e simulador de planos de saúde online" path="/">
       <Nav />
+      <Footer />
     </Page>
   )
 }
