4d4790d7b12461c5940068bc15554002ef609e8a
Icaro Albar <icaro.albar@live.com>
2022-10-13 11:47:40 -0300

    Info2 Criado

---
+++ components/Info2.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function Info2() {
+    return (
+        <div className="bg-gray-100 text-center">
+            <div className="w-11/12 xl:w-7/12 py-10 mx-auto">
+                <h4 className="text-md font-medium uppercase">atendimento grande rio e capital</h4>
+                <h1 className="text-3xl xl:text-5xl font-light py-6">Uma equipe preparada e a melhor consultoria do Rio de Janeiro</h1>
+                <div className="md:w-8/12 mx-auto mt-5">
+                    <p className="md:text-xl font-light">Consulte conosco a linha completa Amil Familiar, Adesão, Empresarial e Odontológica.
+                        Desconto especial para novos clientes e cotação online com nossa equipe.
+                    </p>
+                </div>
+                <Image src='/images/testeimage.jpg' width={500} height={300} />
+                <Image src='/images/testeimage.jpg' width={500} height={300} />
+            </div>
+        </div >
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,7 @@ import Nav from '../components/Nav'
 import Header from '../components/Header'
 import Info1 from '../components/Info1'
 import Cards from '../components/Cards'
+import Info2 from '../components/Info2'
 import Footer from '../components/Footer'
 import Contato from '../components/Contato'
 
@@ -14,6 +15,7 @@ export default function Home() {
       <Header />
       <Info1 />
       <Cards />
+      <Info2 />
       <Contato />
       <Footer />
     </Page>
