6f58dfbf9cc5e52a92b9ed937e8fb083662831b0
Icaro Albar <icaro.albar@live.com>
2022-10-19 13:10:03 -0300

    correção contato responsivo

--- pages/contato.jsx
+++ pages/contato.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
   return (
     <Page title="Simule seu plano" description="Todos os planos de saúde amil e simulador de planos de saúde online" path="/">
       <Nav />
-      <Contato button='simule agora' height='h-screen' display='block' />
+      <Contato button='simule agora' height='lg:h-screen' display='block' />
       <Footer />
     </Page>
   )
