e5a288c057a9c9a21c09ce0bc1f61e9bdbb91673
Icaro Albar <45662231+icaroalbar@users.noreply.github.com>
2022-10-24 15:50:25 -0300

    Correção texto preço

--- components/Info1.jsx
+++ components/Info1.jsx
@@ -6,7 +6,7 @@ export default function Info1() {
         <div className="bg-gray-50">
             <div className="grid grid-cols-2 gap-x-4">
                 <div className="col-span-2 lg:col-span-1 contatoImage3 bg-right"></div>
-                <div className="col-span-2 lg:col-span-1 w-3/4 mx-auto text-center py-16">
+                <div className="col-span-2 lg:col-span-1 w-3/4 mx-auto py-16">
 
                     <p className="uppercase border-b-4 pb-2 text-lg border-primary text-center lg:text-left">simulador de planos amil saúde rio</p>
                     <h1 className="text-3xl mt-10 font-light lg:text-left">Planos a partir de</h1>
@@ -16,11 +16,10 @@ export default function Info1() {
                         <span className="relative bottom-16 sm:bottom-28 text-3xl">,00</span>
                     </div>
 
-                    <p className="text-3xl font-light lg:text-end mb-5 lg:pr-3">Economize até 40% </p>
-                    <p className="text-center text-gray-600 text-xl">Valores para o plano de saúde na linha empresarial Amil Fácil.</p>
-                    <p className="text-center text-gray-600 text-xl">*De 0 à 18 Anos para o Rio de Janeiro</p>
-
-                    <div className="mt-10">
+                    <p className="text-3xl font-light lg:text-center mb-5 lg:pr-3">Reduza o valor do seu plano em até 40%*</p>
+                    <p className="text-gray-600 text-sm text-center">*Valores para o plano de saúde na linha empresarial Amil Fácil.</p>
+                    <p className="text-gray-600 text-sm text-center">De 0 à 18 Anos para o Rio de Janeiro</p>
+                    <div className="mt-10 text-center">
                         <Link
 
                             to="contato"
